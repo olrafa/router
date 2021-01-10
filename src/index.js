c3aa9df0d70da872b08bbf0de2153e2f07547ca5
rafaelrolivares <rafaelrolivares@gmail.com>
2021-01-10 17:55:13 +0100

    playing with tools

--- src/index.js
+++ src/index.js
@@ -16,4 +16,37 @@ leaflet.tileLayer(osmMapnik, {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-leaflet.Control.geocoder().addTo(map);
+// const router = leaflet.Routing.control({
+//   waypoints: [
+//     leaflet.latLng(-34.75998928571429, -58.39461882653061),
+//     leaflet.latLng(-34.6013566, -58.370832375605474)
+//   ]
+// });
+// router.addTo(map);
+
+const geocoder = leaflet.Control.geocoder({ defaultMarkGeocode: false });
+
+const addresses = [];
+
+geocoder.on('markgeocode', e => {
+  addresses.push(e.geocode.center);
+  console.log(e, e.geocode.center);
+  // const waypoint = leaflet.latLng(e.geocode.center.lat, e.geocode.center.lng);
+  // router.options.waypoints.push(waypoint);
+  console.log(leaflet.marker([e.geocode.center.lat, e.geocode.center.lng]));
+  leaflet.marker([e.geocode.center.lat, e.geocode.center.lng]).addTo(map);
+  // console.log(e);
+  // console.log(addresses);
+  const bbox = e.geocode.bbox;
+  const poly = leaflet.polygon([
+    bbox.getSouthEast(),
+    bbox.getNorthEast(),
+    bbox.getNorthWest(),
+    bbox.getSouthWest()
+  ]);
+  map.fitBounds(poly.getBounds());
+});
+
+geocoder.addTo(map);
+
+// console.log(router);
